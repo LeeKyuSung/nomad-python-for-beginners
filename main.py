8426e61aaed52ec8a00b5fb9b156f1a3ab866084
LeeKyuSung <ks5050577@gmail.com>
2023-02-19 20:37:37 +0900

    #5.8 saving results

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-def make_juice(fruit):
-    return f"{fruit}+🥤"
+from requests import get
+from bs4 import BeautifulSoup
 
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
 
-def add_ice(juice):
-    return f"{juice}+🧊"
-
-
-def add_sugar(iced_juice):
-    return f"{iced_juice}+🍬"
-
-
-juice = make_juice("🍎")
-cold_juice = add_ice(juice)
-perfect_juice = add_sugar(cold_juice)
-print(perfect_juice)
-
-winner = 15
-if winner > 10:
-    print("winner is greater tan 10")
-elif winner < 10:
-    print("winner is less than 10")
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+    print("status_code : " + response.status_code)
 else:
-    print("winner is 10")
+    results = []
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all("section", class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all("li")
+        job_posts.pop()
+        for post in job_posts:
+            anchors = post.find_all("a")
+            anchor = anchors[1]
+            company, kind, region = anchor.find_all("span", class_="company")
+            title = anchor.find("span", class_="title")
+            job_data = {
+                'title': title.string,
+                'company': company.string,
+                'region': region.string,
+                'position': title.string
+            }
+            results.append(job_data)
+    for result in results:
+        print(result)
