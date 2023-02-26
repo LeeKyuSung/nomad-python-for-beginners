2e8c5bcd01f1d46a8a543d53d909d12e530e424a
LeeKyuSung <ks5050577@gmail.com>
2023-02-26 15:01:19 +0900

    Pyhton으로 웹 스크래퍼 만들기 수강 완료

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, send_file
+from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
+
+db = {}
 
 
 @app.route("/")
 def home():
-    return "Welcome to JobScrapper"
+    return render_template("home.html")
+
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    if keyword is None:
+        return redirect("/")
+    if keyword in db:
+        jobs = db[keyword]
+    else:
+        jobs = extract_wwr_jobs(keyword)
+        db[keyword] = jobs
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword is None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run()
