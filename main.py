82a25aaf8807b55e32e36f137db06759af56ab9e
LeeKyuSung <ks5050577@gmail.com>
2023-02-26 13:40:09 +0900

    5.12 Indeed 403 Fix

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
 from requests import get
 from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-base_url = "https://weworkremotely.com/remote-jobs/search?term="
-search_term = "python"
-
-response = get(f"{base_url}{search_term}")
-if response.status_code != 200:
-    print("status_code : " + response.status_code)
-else:
-    results = []
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all("section", class_="jobs")
-    for job_section in jobs:
-        job_posts = job_section.find_all("li")
-        job_posts.pop()
-        for post in job_posts:
-            anchors = post.find_all("a")
-            anchor = anchors[1]
-            company, kind, region = anchor.find_all("span", class_="company")
-            title = anchor.find("span", class_="title")
-            job_data = {
-                'title': title.string,
-                'company': company.string,
-                'region': region.string,
-                'position': title.string
-            }
-            results.append(job_data)
-    for result in results:
-        print(result)
+browser = webdriver.Chrome()
+browser.get("https://www.indeed.com/jobs?q=python&l=Remote&limit=50")
+print(browser.page_source)
